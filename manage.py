0c19e103aa1c9673bde902ae3604dd6c5c6c8cb1
Evgeny Akimenko <evg.akimenko@gmail.com>
2023-07-11 19:30:16 +0300

    docker-compose

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import environ
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoomBooking_test.settings')
+    environ.Env.read_env()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
